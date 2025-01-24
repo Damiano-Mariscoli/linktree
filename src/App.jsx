f01f9b6b1072facdbabd8456dbe99bc674e181cc
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-01-24 01:00:29 +0100

    ADDED: link tree theme work

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import DefaultLayout from "./layouts/defaultLayout";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 import About from "./pages/about";
-
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div
+      className=" "
+      style={{
+        height: "100vh",
+        background:
+          "linear-gradient(to top, rgb(55, 65, 81), rgb(17, 24, 39), rgb(0, 0, 0))",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,60 @@
 export default function HomePage() {
+  const links = [{}];
   return (
     <>
-      <div></div>
+      <div className="container animate-load">
+        <div className="homeHeader mb-3">
+          <img className="user" src="user.svg" alt="" />
+          <h1 className="userName">Damiano Mariscoli</h1>
+        </div>
+
+        <div className="flex flex-col items-center justify-center row gap-3 mx-auto">
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="github.svg" alt="" />
+            <button className="iconName ">Github</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="curriculum.svg" alt="" />
+            <button className="iconName ">Curriculum</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="portfolio.svg" alt="" />
+            <button className="iconName ">Portfolio</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="linkedin.svg" alt="" />
+            <button className="iconName ">Linkedin</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="gmail.svg" alt="" />
+            <button className="iconName ">Email</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="blog.svg" alt="" />
+            <button className="iconName ">Blog</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="instagram.svg" alt="" />
+            <button className="iconName ">Instagram</button>
+          </div>
+
+          <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="twitter.svg" alt="" />
+            <button className="iconName ">X</button>
+          </div>
+
+          {/* <div className="col-1 d-flex iconButton justify-center gap-1">
+            <img className="icon" src="stackOverflow.svg" alt="" />
+            <button className="iconName ">SO</button>
+          </div> */}
+        </div>
+      </div>
     </>
   );
 }
